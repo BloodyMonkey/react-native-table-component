77d892b6aabe4e2c612246d185dbab2b3a296532
BloodyMonkey <65278563+BloodyMonkey@users.noreply.github.com>
2021-11-04 15:11:14 +0100

    Cell web update

--- components/cell.js
+++ components/cell.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ViewPropTypes, Text, StyleSheet } from 'react-native';
 
 export class Cell extends Component {
 
